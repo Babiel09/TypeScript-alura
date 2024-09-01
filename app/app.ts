d783eddf682e722a383178009f6a99d0e92bc0de
Babiel09 <estudosbabiel@gmail.com>
2024-09-01 17:34:19 -0300

    Vamo vê se agora pega

--- app/app.ts
+++ app/app.ts
@@ -3,7 +3,7 @@ import { NegociacaoController } from './controllers/controller-nec.js';
 const controller = new NegociacaoController
 //Criando o formulário:
 
-const form = document.querySelector('.form-group')
+const form = document.querySelector('.form')
 form.addEventListener('sumbit', event=>{
     event.preventDefault()
     controller.soma()
