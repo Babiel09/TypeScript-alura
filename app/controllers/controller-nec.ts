0829635cd9e15ece8ac00ba4e2c3f8e7b5e10ab9
Babiel09 <estudosbabiel@gmail.com>
2024-09-03 19:23:22 -0300

    Ajuste no private

--- app/controllers/controller-nec.ts
+++ app/controllers/controller-nec.ts
@@ -2,16 +2,16 @@ import { Negociacoes } from "../models/lista-negociacoes/lista-negocio.js"
 import { Negociacao } from "../models/negociacao.js"
 
 export class NegociacaoController{
-    private _inputData: HTMLInputElement
-    private _inputQuantidade: HTMLInputElement
-    private _inputValor: HTMLInputElement  
-    private _negociacoes = new Negociacoes()
+    private inputData: HTMLInputElement
+    private inputQuantidade: HTMLInputElement
+    private inputValor: HTMLInputElement  
+    private negociacoes = new Negociacoes()
     
     constructor(){
         //Armazenando os valores dentro dos inputs
-        this._inputData = document.querySelector('#data')
-        this._inputQuantidade = document.querySelector('#quantidade')
-        this._inputValor = document.querySelector('#valor')
+        this.inputData = document.querySelector('#data')
+        this.inputQuantidade = document.querySelector('#quantidade')
+        this.inputValor = document.querySelector('#valor')
     }
 
     //Soma:
@@ -22,11 +22,11 @@ export class NegociacaoController{
     //Criando método para add o valor que tem lá dentro com o console.log:
     dadosNegociacao(): Negociacao{
         const exp = /-/g
-        const data = new Date(this._inputData.value.replace(exp, ','))
-        const quantidade = parseInt(this._inputQuantidade.value)
-        const valor = parseFloat(this._inputValor.value)
+        const data = new Date(this.inputData.value.replace(exp, ','))
+        const quantidade = parseInt(this.inputQuantidade.value)
+        const valor = parseFloat(this.inputValor.value)
         const negocia = new Negociacao(data, quantidade, valor)
-        this._negociacoes.lista()
+        this.negociacoes.lista()
         this.limparForm()
         console.log(negocia)
         return negocia
@@ -34,9 +34,9 @@ export class NegociacaoController{
     }
     
     limparForm(): void{
-        this._inputData.value = ''
-        this._inputQuantidade.value = ''
-        this._inputValor.value = ''
-        this._inputData.focus()
+        this.inputData.value = ''
+        this.inputQuantidade.value = ''
+        this.inputValor.value = ''
+        this.inputData.focus()
     }
 }
