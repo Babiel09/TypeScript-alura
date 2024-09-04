f5e35c8f9a3d71a350bd9a8fe11c5d914b65b8be
Babiel09 <estudosbabiel@gmail.com>
2024-09-04 20:37:33 -0300

    Protegendo o template

--- app/views/view.ts
+++ app/views/view.ts
@@ -6,10 +6,13 @@ export abstract class View<T> {
         this.elemento = document.querySelector(seletor);
     }
 
-    update(model: T): void {
-        const template = this.template(model);
+    public update(model: T): void {
+        const template = this.template(model);  
         this.elemento.innerHTML = template;
     }
-
-    abstract template(model: T): string;
+    //Para fazer com que ninguém fora das filhas e do mãe possa acessar o template para não o modificar, eu utilizo:
+    protected abstract template(model: T): string;
+    //Com o "protect abstract" somente as filhas dessa classe mãe podem acessar o template, 
+    //o grande problema que aqui na classe mãe o template está protegido, mas na classe filha não.
+    //Para evitar isso eu devo colocar protected em todas as classes filhas
 }
