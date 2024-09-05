b01b1bcdae10c8ac81ece6e58d5641eb5c71a518
Babiel09 <estudosbabiel@gmail.com>
2024-09-05 15:02:26 -0300

    Limpando o código

--- app/enumaration/dias-da-semana.ts
+++ app/enumaration/dias-da-semana.ts
@@ -1,9 +1,9 @@
 export enum DiasDaSemana{
-    DOMINGO=  0,    //0 DOMINGO
-    SEGUNDA = 1,    //1 SEGUNDA
-    TERÇA =   2,     //2 TERÇA
-    QUARTA =  3,     //3 QUARTA
-    QUINTA =  4,     //4 QUINTA
-    SEXTA =   5,     //5 SEXTA
-    SÁBADO =  6     //6 SÁBADO
+    DOMINGO= 0,    //0 DOMINGO
+    SEGUNDA= 1,    //1 SEGUNDA
+    TERÇA=   2,    //2 TERÇA
+    QUARTA=  3,    //3 QUARTA
+    QUINTA=  4,    //4 QUINTA
+    SEXTA=   5,    //5 SEXTA
+    SÁBADO=  6     //6 SÁBADO
 }
--- app/views/view.ts
+++ app/views/view.ts
@@ -10,9 +10,9 @@ export abstract class View<T> {
         const template = this.template(model);  
         this.elemento.innerHTML = template;
     }
-    //Para fazer com que ninguém fora das filhas e do mãe possa acessar o template para não o modificar, eu utilizo:
+//Para fazer com que ninguém fora das filhas e do mãe possa acessar o template para não o modificar, eu utilizo:
     protected abstract template(model: T): string;
-    //Com o "protect abstract" somente as filhas dessa classe mãe podem acessar o template, 
-    //o grande problema que aqui na classe mãe o template está protegido, mas na classe filha não.
-    //Para evitar isso eu devo colocar protected em todas as classes filhas
+        //Com o "protect abstract" somente as filhas dessa classe mãe podem acessar o template, 
+            //o grande problema que aqui na classe mãe o template está protegido, mas na classe filha não.
+             //Para evitar isso eu devo colocar protected em todas as classes filhas
 };
